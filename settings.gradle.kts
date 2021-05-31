2910eede3fb2829f1e5329f622155e68699e64c4
Nick Cipollo <njc1115@gmail.com>
2021-05-31 17:35:42 -0400

    move cli into subfolder

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,4 @@ import java.net.URI
 
 rootProject.name = "tix-cli"
 
-sourceControl {
-    gitRepository(URI("https://github.com/tixorg/tix-core.git")) {
-        producesModule("org.tix:core")
-    }
-}
+include(":cli")
