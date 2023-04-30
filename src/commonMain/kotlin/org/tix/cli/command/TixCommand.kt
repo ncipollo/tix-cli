dfdda870c3069a9173aa5c35c6655b0c38fd2399
Nick Cipollo <njc1115@gmail.com>
2023-04-30 17:47:45 -0400

    Add optional path

--- src/commonMain/kotlin/org/tix/cli/command/TixCommand.kt
+++ src/commonMain/kotlin/org/tix/cli/command/TixCommand.kt
@@ -2,13 +2,18 @@ package org.tix.cli.command
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.optional
 import com.github.ajalt.clikt.parameters.options.versionOption
 
-class TixCommand: CliktCommand() {
+class TixCommand: CliktCommand(invokeWithoutSubcommand = true) {
+    private val path by argument().optional()
     init {
         versionOption("🚀 0.9.7")
     }
 
     override fun run() {
+        if (currentContext.invokedSubcommand == null) {
+            echo("Provided path: $path")
+        }
     }
 }
