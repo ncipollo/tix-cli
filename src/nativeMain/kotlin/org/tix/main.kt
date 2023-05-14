22853b4c0bb73e762ff80bb6f645c5fb41f92b9c
Nick Cipollo <njc1115@gmail.com>
2023-05-14 13:27:13 -0400

    Remove old main function

--- src/nativeMain/kotlin/org/tix/main.kt
+++ src/nativeMain/kotlin/org/tix/main.kt
@@ -1,39 +1,9 @@
 package org.tix
 
 import com.github.ajalt.clikt.core.subcommands
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import org.tix.builder.tixPlanForCLI
-import org.tix.cli.command.plan.PlanCommand
 import org.tix.cli.command.TixCommand
-import org.tix.feature.plan.presentation.PlanViewEvent
+import org.tix.cli.command.plan.PlanCommand
 
 fun main(args: Array<String>) = TixCommand()
     .subcommands(PlanCommand())
     .main(args)
-
-private fun oldMain() {
-    val path =  "~/Desktop/tix/story.md"
-    runBlocking {
-        println("in blocking")
-        coroutineScope {
-            val plan = tixPlanForCLI()
-            val viewModel = plan.planViewModel(this)
-            var job: Job? = null
-            job = launch {
-                viewModel.viewState.collect {
-                    if (it.isComplete) {
-                        job?.cancel()
-                    }
-                }
-            }
-            launch {
-                viewModel.sendViewEvent(
-                    PlanViewEvent.PlanUsingMarkdown(path = path, shouldDryRun = true)
-                )
-            }
-        }
-    }
-}
