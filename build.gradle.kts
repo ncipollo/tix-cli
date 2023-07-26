e0a232e838cd6c046722c7e1808c578fabc9368b
Nick Cipollo <njc1115@gmail.com>
2023-07-26 18:31:09 -0400

    Bump gradle wrapper version

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ kotlin {
                 }
 
                 implementation("com.github.ajalt.clikt:clikt:3.5.2")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
             }
         }
         val commonTest by getting
